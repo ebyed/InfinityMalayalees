6283212b1f17ab473e983c34b3b64f943fbf8862
Eby <ebyed.c@gmail.com>
2025-07-05 17:40:08 +0530

    Fix KeralaLogoIcon import path

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { Home, Calendar, Settings, Info } from 'lucide-react';
-import { KeralaLogoIcon } from './KeralaSVGIcons';
+import { KeralaLogoIcon } from './components/KeralaSVGIcons';
 
 const Navigation: React.FC = () => {
   const location = useLocation();
