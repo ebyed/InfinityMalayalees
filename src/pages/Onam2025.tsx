0bd235b82e96d9dc35d24b8d43fa17dbbb52ff7a
Eby <ebyed.c@gmail.com>
2025-07-18 15:21:44 +0530

    Updated Onam2025.tsx

--- src/pages/Onam2025.tsx
+++ src/pages/Onam2025.tsx
@@ -71,8 +71,8 @@ const Onam2025: React.FC = () => {
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex flex-wrap justify-center gap-4 mb-8">
             <TabButton tabId="overview" label="Event Schedule" icon={Calendar} />
-            <TabButton tabId="events" label="Register for Cultural Events - 13th September" icon={() => <CulturalIcon size={18} />} />
-            <TabButton tabId="thiruvathira" label="Register for Mega Thiruvathira - 14th September" icon={() => <ThiruvathiraIcon size={18} />} />
+            <TabButton tabId="events" label="Register for Cultural Events" icon={() => <CulturalIcon size={18} />} />
+            <TabButton tabId="thiruvathira" label="Register for Mega Thiruvathira" icon={() => <ThiruvathiraIcon size={18} />} />
             <TabButton tabId="sadya" label="Sadya Information" icon={() => <SadyaIcon size={18} />} />
           </div>
         </div>
